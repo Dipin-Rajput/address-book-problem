e9e44961c4f664aa80a206ad23b2fae70f12624e
Dipin-Rajput India <dipinrajput707@gmail.com>
2025-02-16 15:26:37 +0530

    [Dipin]feat: Add ability to add a new contact to Address Book (UC2)

--- addressBookProblem.cpp
+++ addressBookProblem.cpp
@@ -1,6 +1,16 @@
 # include <iostream>
+# include <vector>
+# include <string>
 using namespace std;
 
+class AddressBookMain{
+
+public:
+    void displayMessage(){ // function to display message
+        cout << "\n-------------------- Welcome to Address Book Program --------------------\n";
+    }
+};
+    
 class Contact{
 
     string firstName, lastName, fullName, address, city, state, email;
@@ -24,7 +34,7 @@ public:
         this->fullName = firstName + " " + lastName;
     }
 
-    void displayContact(){
+    void displayContact() const{ // Function to display contact
 
         cout << "\nFull Name: " << fullName << endl;
         cout << "Address: " <<  address << endl;
@@ -36,50 +46,57 @@ public:
     }
 };
 
-class AddressBookMain{
+class AddressBook{
+
+    vector<Contact> addressBook;
 
 public:
-    void displayMessage(){ // function to display message
-        cout << "\n-------------------- Welcome to Address Book Program --------------------\n";
+
+    // UC2: Ability to add new contact in address book
+
+    void addContact(){
+
+        string firstName, lastName, address, city, state, email;
+        int zip, phoneNumber;
+
+        // User - Input
+
+        cout << "\nEnter your first name: "; cin >> firstName;
+        cout << "Enter your last name: ";  cin >> lastName;
+
+        cin.ignore(); // Function to clear the newline character ('\n')
+
+        cout << "Enter your address: "; getline(cin, address);
+        cout << "Enter your city: "; getline(cin, city);
+        cout << "Enter your state: "; getline(cin, state);
+        cout << "Enter your zip: "; cin >> zip;
+        cout << "Enter your phone number: "; cin >> phoneNumber;
+
+        cin.ignore(); // Function to clear the newline character ('\n')
+
+        cout << "Enter your email: "; getline(cin, email);
+
+        Contact newContact(firstName, lastName, address, city, state, zip, phoneNumber, email); // Object for Contact class
+        addressBook.push_back(newContact); // Add contact to addressBook vector
+    }
+
+    void displayContacts(){
+
+        for(const auto& contact : addressBook){ // Loop to iterate over addressBook
+            contact.displayContact(); // Fucntion call to display contact
+        }
     }
 };
 
+
 int main(){
 
-    AddressBookMain msg; // Object for AddresssBookMain class
-    msg.displayMessage(); // Function call to display message
+    AddressBookMain addressBookMain; // Object for AddresssBookMain class
+    addressBookMain.displayMessage(); // Function call to display message
 
-    string firstName, lastName, address, city, state, email;
-    int zip, phoneNumber;
-
-    // User - Input
-
-    cout << "\nEnter your first name: ";
-    cin >> firstName;
-    cout << "Enter your last name: ";
-    cin >> lastName;
-
-    cin.ignore(); // function to clear the newline character ('\n')
-
-    cout << "Enter your address: ";
-    getline(cin, address);
-    cout << "Enter your city: ";
-    getline(cin, city);
-    cout << "Enter your state: ";
-    getline(cin, state);
-    cout << "Enter your zip: ";
-    cin >> zip;
-    cout << "Enter your phone number: ";
-    cin >> phoneNumber;
-
-    cin.ignore(); // function to clear the newline character ('\n')
-
-    cout << "Enter your email: ";
-    getline(cin, email);
-
-    Contact contact(firstName, lastName, address, city, state, zip, phoneNumber, email); // Object for Contact class
-
-    contact.displayContact(); // fucntion call to display contact
+    AddressBook addressBook; // Object for AdressBook class
+    addressBook.addContact(); // Function call to add contact
+    addressBook.displayContacts(); // Function call to display contacts
 
     return 0;
 }
