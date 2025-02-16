7ad54038f9e0fddb321fb3d6ba3aa9bd62e2713a
Dipin-Rajput India <dipinrajput707@gmail.com>
2025-02-16 18:00:04 +0530

    [Dipin]feat: Add ability to edit an existing contact in address book (UC3)

--- addressBookProblem.cpp
+++ addressBookProblem.cpp
@@ -13,14 +13,13 @@ public:
     
 class Contact{
 
-    string firstName, lastName, fullName, address, city, state, email;
-    int zip, phoneNumber;
-
 public:
+
+    string firstName, lastName, fullName, address, city, state, zip, phoneNumber, email;
 
     // UC1: Ability to create contact in address book
 
-    Contact(string firstName, string lastName, string address, string city, string state, int zip, int phoneNumber, string email){
+    Contact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email){
 
         this->firstName = firstName;
         this->lastName = lastName;
@@ -42,7 +41,7 @@ public:
         cout << "State: " <<  state << endl;
         cout << "Zip: " <<  zip << endl;
         cout << "Phone Number: " <<  phoneNumber << endl;
-        cout << "Email: " <<  email << "\n" << endl;
+        cout << "Email: " <<  email << endl;
     }
 };
 
@@ -56,8 +55,7 @@ public:
 
     void addContact(){
 
-        string firstName, lastName, address, city, state, email;
-        int zip, phoneNumber;
+        string firstName, lastName, address, city, state, zip, phoneNumber, email;
 
         // User - Input
 
@@ -80,7 +78,60 @@ public:
         addressBook.push_back(newContact); // Add contact to addressBook vector
     }
 
+    // UC3: Ability to edit contact in address book
+
+    void editContact(){
+
+        if(addressBook.empty()){ // Check if addressBook is empty or not
+            cout << "\nAddress Book is empty, please add contact first";
+            return;
+        }
+
+        string firstName, lastName, fullName, input;
+
+        cout << "\nEnter your first name: "; cin >> firstName;
+        cout << "Enter your last name: "; cin >> lastName;
+        fullName = firstName + " " + lastName;
+
+        for(auto& contact : addressBook){ // Loop to find the contact with name
+            if(contact.fullName == fullName){
+
+                cout << "\n-------------------- Editing contact details for '" << fullName << "' --------------------";
+                cout << "\n-------------------- Press enter to keep current details --------------------\n";
+
+                cout << "\nEnter new address [current address: "<< contact.address << "]: "; cin.ignore(); getline(cin, input);
+                if(!input.empty()){contact.address = input;}
+
+                cout << "Enter new city [current city: "<< contact.city << "]: "; getline(cin, input);
+                if(!input.empty()){contact.city = input;}
+
+                cout << "Enter new state [current state: "<< contact.state << "]: "; getline(cin, input);
+                if(!input.empty()){contact.state = input;}
+
+                cout << "Enter new zip [current zip: "<< contact.zip << "]: "; cin >> contact.zip;
+                if(!input.empty()){contact.zip = input;}
+
+                cout << "Enter new phone number [current phone number: "<< contact.phoneNumber<< "]: "; cin >> contact.phoneNumber;
+                if(!input.empty()){contact.phoneNumber = input;}
+
+                cout << "Enter new email [current email: "<< contact.email << "]: "; cin.ignore(); getline(cin, input);
+                if(!input.empty()){contact.email = input;}
+
+                cout << "\nContact updated successfully\n";
+
+                return;
+            }
+        }
+
+        cout << "\nContact Not found, please enter valid name\n";
+    }
+
     void displayContacts(){
+
+        if(addressBook.empty()){ // Check if addressBook is empty or not
+            cout << "\nAddress Book is empty, please add contact first";
+            return;
+        }
 
         for(const auto& contact : addressBook){ // Loop to iterate over addressBook
             contact.displayContact(); // Fucntion call to display contact
@@ -95,8 +146,11 @@ int main(){
     addressBookMain.displayMessage(); // Function call to display message
 
     AddressBook addressBook; // Object for AdressBook class
+
     addressBook.addContact(); // Function call to add contact
-    addressBook.displayContacts(); // Function call to display contacts
+    addressBook.displayContacts(); // Function call to display contact
+    addressBook.editContact(); // Function call to edit contact
+    addressBook.displayContacts(); // Function call to display contact after edit
 
     return 0;
 }
